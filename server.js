11dde9e0752dda19e49dd484dd011a673a1bc066
Jimmy Gong <gong.jim@gmail.com>
2015-01-30 23:07:50 -0800

    Serves static files

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+
+//Serve static files
+app.use(express.static(__dirname + '/client'));
+app.use(express.static(__dirname + '/client/styles'));
 
 app.get('/', function(req, res){
   res.send('Hello, World!');
 });
 
-var server = app.listen(8000, function(){
-  var host = server.address().address;
-  var port = server.address().port;
 
-  console.log('Listening at http://%s:%s', host, port);
-})
+var port = process.env.PORT || 8000;
+http.listen(port, function() {
+  console.log('Listening at http://localhost:%s', port);
+});
