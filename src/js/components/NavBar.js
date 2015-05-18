9709c80392ea004e6189c116e7af1db7a3fcd46f
Jimmy Gong <gong.jim@gmail.com>
2015-05-17 19:20:15 -0700

    Adds NavBar component

---
+++ src/js/components/NavBar.js
@@ -0,0 +1,22 @@
+var React = require('react');
+var Router = require('react-router');
+var RouteHandler = Router.RouteHandler;
+
+var mui = require('material-ui');
+
+var NavBar = React.createClass({
+  /** 
+   * get:
+   *    isLoggedIn
+   *    sessionId (if logged in)
+   *    networks (if logged in)
+   */
+  getInitialState: function(){
+
+  },
+  render: function(){
+    
+  }
+});
+
+module.exports = NavBar;
