cb255b5412f77b6ad9d097f3db419a6fcc515930
Jimmy Gong <gong.jim@gmail.com>
2015-05-18 15:35:38 -0700

    Builds out some of NavBar component

--- src/js/components/NavBar.js
+++ src/js/components/NavBar.js
@@ -3,18 +3,35 @@ var Router = require('react-router');
 var RouteHandler = Router.RouteHandler;
 
 var mui = require('material-ui');
+var FlatButton = mui.FlatButton;
+
+var AuthStore = require('../stores/AuthStore');
+function getUserState(){
+  return {
+    username: AuthStore.getUsername(),
+    networks: AuthStore.getNetworks()
+  };
+}
 
 var NavBar = React.createClass({
-  /** 
-   * get:
-   *    isLoggedIn
-   *    networks (if logged in)
-   */
   getInitialState: function(){
-
+    return getUserState()
+  },
+  componentDidMount: function(){
+    BlogStore.addChangeListener(this._onChange);
+  },
+  componentWillUnmount: function(){
+    BlogStore.removeChangeListener(this._onChange);
+  },
+  _onChange: function(){
+    this.setState(getBlogState());
   },
   render: function(){
-    
+    return (
+      <div>
+        <RouteHandler />
+      </div>
+    );
   }
 });
 
